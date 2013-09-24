fa8de3f4acd1d5c4cca996bd46b85c48d7a76bac
James Reeves <jreeves@weavejester.com>
2013-09-24 11:01:57 +0100

    Different types for RGB and RGBA colors

--- src/crumpets/core.clj
+++ src/crumpets/core.clj
@@ -9,20 +9,31 @@
 (defn- from-hex [hex]
   (Integer/parseInt hex 16))
 
-(defrecord Color [red green blue alpha]
+(defrecord ColorRGB [red green blue]
+  Object
+  (toString [_]
+    (str "#color \"" (to-hex red) (to-hex green) (to-hex blue) "\"")))
+
+(defrecord ColorRGBA [red green blue alpha]
   Object
   (toString [_]
     (str "#color \"" (to-hex red) (to-hex green) (to-hex blue) (to-hex alpha) "\"")))
 
-(defmethod print-method Color [^Color c ^java.io.Writer w]
+(defmethod print-method ColorRGB [^ColorRGB c ^java.io.Writer w]
   (.write w (.toString c)))
 
-(defmethod print-dup Color [^Color c ^java.io.Writer w]
+(defmethod print-dup ColorRGB [^ColorRGB c ^java.io.Writer w]
+  (.write w (.toString c)))
+
+(defmethod print-method ColorRGBA [^ColorRGBA c ^java.io.Writer w]
+  (.write w (.toString c)))
+
+(defmethod print-dup ColorRGBA [^ColorRGBA c ^java.io.Writer w]
   (.write w (.toString c)))
 
 (defn hex->color [hex-string]
   (case (count hex-string)
     8 (let [[r g b a] (map from-hex (re-seq #".." hex-string))]
-        (->Color r g b a))
+        (->ColorRGBA r g b a))
     6 (let [[r g b] (map from-hex (re-seq #".." hex-string))]
-        (->Color r g b 255))))
+        (->ColorRGB r g b))))
