84d11180021089973a23bc758dc0ec823d8f6cb0
James Reeves <jreeves@weavejester.com>
2013-09-24 13:26:30 +0100

    Added some reader and printer tests

--- test/crumpets/core_test.clj
+++ test/crumpets/core_test.clj
@@ -2,6 +2,35 @@
   (:require [clojure.test :refer :all]
             [crumpets.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest reader-test
+  (testing "hex string"
+    (is (= (read-string "#color \"#ff9900aa\"")
+           (->ColorRGBA 255 153 0 170)))
+    (is (= (read-string "#color \"ff9900aa\"")
+           (->ColorRGBA 255 153 0 170)))
+    (is (= (read-string "#color \"#ff9900\"")
+           (->ColorRGB 255 153 0)))
+    (is (= (read-string "#color \"ff9900\"")
+           (->ColorRGB 255 153 0))))
+  (testing "int vector"
+    (is (= (read-string "#color [255 153 0 170]")
+           (->ColorRGBA 255 153 0 170)))
+    (is (= (read-string "#color [255 153 0]")
+           (->ColorRGB 255 153 0))))
+  (testing "float vector"
+    (is (= (read-string "#color [1.0 0.6 0.0 0.4]")
+           (->ColorRGBA 255 153 0 102)))
+    (is (= (read-string "#color [1.0 0.6 0.0]")
+           (->ColorRGB 255 153 0)))))
+
+(deftest print-test
+  (is (= (pr-str (->ColorRGBA 255 153 0 170))
+         "#color \"#ff9900aa\""))
+  (is (= (pr-str (->ColorRGB 255 153 0))
+         "#color \"#ff9900\"")))
+
+(deftest ->color-test
+  (is (= (->color [255 153 0 170])
+         (->ColorRGBA 255 153 0 170)))
+  (is (= (->color [1.0 0.0 0.6])
+         (->ColorRGB 255 0 153))))
