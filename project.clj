76e2fc3d628e450820073e71c2c5e3ab509578d6
James Reeves <jreeves@weavejester.com>
2013-09-25 08:58:46 +0100

    Added URL and description to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject crumpets "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Library for representing and manipulating color data"
+  :url "https://github.com/weavejester/crumpets"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
