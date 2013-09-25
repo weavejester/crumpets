cd766ab7c5e3314cfe351eb6b823bf435cddb454
James Reeves <jreeves@weavejester.com>
2013-09-25 14:10:43 +0100

    Added Clojure 1.5 profile for testing

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :plugins [[codox "0.6.5"]])
+  :plugins [[codox "0.6.5"]]
+  :profiles
+  {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
